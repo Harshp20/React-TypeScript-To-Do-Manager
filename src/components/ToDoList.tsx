ae562e2c0ad861770648a565eae34f3e940fad5f
Harsh <harsh.pradhan@ext.zestmoney.in>
2022-08-10 20:58:40 +0530

    List update issue fixed

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react";
+import { useState, useEffect } from "react";
 import ToDo from "./ToDo";
 
 export interface ToDoList {
@@ -10,24 +10,22 @@ export interface ToDoList {
 const ToDoList: React.FC = () => {
   const [desc, setDesc] = useState(() => "");
   const [editToDo, setEditToDo] = useState(() => -1);
-  const inputRef = useRef<any>();
-  const [toDoList, setToDoList] = useState<Array<ToDoList>>(() => [
-    {
-      id: 1,
-      desc: "Feed the cat",
-      status: false,
-    },
-    {
-      id: 2,
-      desc: "Wash the car",
-      status: false,
-    },
-    {
-      id: 3,
-      desc: "Do laundry",
-      status: false,
-    },
-  ]);
+  const [toDoList, setToDoList] = useState<Array<ToDoList>>(() => {
+    const tempToDoList = localStorage.getItem('To Do List')
+    if (tempToDoList?.length) {
+      return JSON.parse(tempToDoList)
+    }
+  });
+
+  useEffect(() => {
+    localStorage.setItem('To Do List', JSON.stringify(toDoList))
+  }, [toDoList])
+
+  useEffect(() => {
+    toDoList.map((toDo) => {
+      return toDo.id === editToDo ? setDesc(toDo.desc) : ""
+    });
+  }, [editToDo])
 
   const addTask = () => {
     if (desc.length === 0) {
@@ -35,7 +33,7 @@ const ToDoList: React.FC = () => {
       return;
     }
 
-    if (toDoList.find((toDo) => toDo.desc.toLowerCase() === desc.toLowerCase()))
+    if (toDoList.find((toDo) => toDo.desc.toLowerCase().trim() === desc.toLowerCase().trim()))
       return;
 
     if (editToDo > 0) {
@@ -43,33 +41,30 @@ const ToDoList: React.FC = () => {
         toDoList.map((toDo) =>
           toDo.id === editToDo
             ? {
-                ...toDo,
-                desc: desc,
-                status: toDo.status ? !toDo.status : toDo.status,
-              }
+              ...toDo,
+              desc: desc.trim(),
+              status: toDo.status ? !toDo.status : toDo.status,
+            }
             : toDo
         )
       );
       setEditToDo(-1);
-    } else
+    } else {
       setToDoList(() => [
-        { id: Math.floor(Math.random() * 100) + 1, desc, status: false },
+        { id: Math.floor(Math.random() * 100) + 1, desc: desc.trim(), status: false },
         ...toDoList,
       ]);
+    }
 
-    inputRef.current.value = "";
+    setDesc('');
   };
 
   const deleteTask = (id: number) => {
-    const newToDoList = toDoList.filter((toDo) => toDo.id !== id);
-    setToDoList(newToDoList);
+    setToDoList(() => toDoList.filter((toDo) => toDo.id !== id));
   };
 
   const updateDesc = (id: number) => {
-    setEditToDo(id);
-    toDoList.map((toDo) =>
-      toDo.id === editToDo ? (inputRef.current.value = toDo.desc) : ""
-    );
+    setEditToDo(id)
   };
 
   const toggleToDo = (id: number) => {
@@ -84,10 +79,10 @@ const ToDoList: React.FC = () => {
     <>
       <h3>Your To Do List</h3>
       <input
-        ref={inputRef}
+        value={desc}
         type="text"
         placeholder="Add a task"
-        onChange={(e) => setDesc(e.target.value.trim())}
+        onChange={(e) => setDesc(e.target.value)}
         onKeyUp={(e) => (e.key === "Enter" ? addTask() : null)}
       />
       <button onClick={addTask}>Add Task</button>
